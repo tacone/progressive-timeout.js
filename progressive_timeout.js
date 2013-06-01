55c5fed48ae72e9364a6007b65b1e8a42ee54a59
tacone <tacone@gmail.com>
2013-06-02 01:35:32 +0200

    Update progressive_timeout.js

--- progressive_timeout.js
+++ progressive_timeout.js
@@ -1,36 +1,11 @@
 /**
- * Will execute the callback with a (factor) increasing delay
- *
- * example (will automatically log to the console every 50 to 4000ms, with an increment of 2x:  
- * <pre>
- * var t = new ProgressiveTimeout(function(){ console.log(':)') }, 50, 4000,2);
- * t.start(); 
- * //.....
- * t.stop(); //will stop looping;
- * t.start(); //will start again with a delay of 50ms, then 100ms, ..
- * //....
- * t.start(); //will execute immediately and then resume from 50ms delay
- * </pre>
+ * @author tacone
+ * @license MIT
  * 
- * example2 (manual cycling)
- * <pre>
- * var t = new ProgressiveTimeout(function(){ console.log(':)') }, 50, 4000,2);
- * t.next(); //will execute immediately
- * t.next(); //will wait 50ms and then execute
- * t.next(); //will wait 100ms and then execute
- * t.reset();
- * t.next(); //will execute immediately
- * t.next(); //will wait 50ms and then execute
- * //...
- * <pre>
+ * Please read the DOCS at https://github.com/tacone/progressive-timeout.js/blob/master/README.md
  * 
- * @param function callback
- * @param integer min
- * @param integer max
- * @param float incrementFactor
- * @returns {ProgressiveTimeout}
+ * This file comes as it is, it could screw up your business or life, I shall not held responsible of anything.
  */
-
 var ProgressiveTimeout = function(callback, min, max, incrementFactor)
 {
     this.callback = callback;
